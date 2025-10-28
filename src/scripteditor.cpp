ed823c8c172e2fb3129371ef2256a990f418d2bf
Rourugin <maks.4min@gmail.com>
2025-10-28 11:28:05 +0300

    setup fromats of text

--- src/scripteditor.cpp
+++ src/scripteditor.cpp
@@ -22,5 +22,41 @@ void ScriptEditor::SetupUI()
 
 void ScriptEditor::SetupFormatting()
 {
+    //animation
+    QTextCharFormat AnimationFormat;
+    AnimationFormat.setBackground(QColor(183, 90, 90));
+    textFormats["animation"] = AnimationFormat;
 
+    //video
+    QTextCharFormat VideoFormat;
+    VideoFormat.setBackground(QColor(103, 185, 184));
+    VideoFormat.setFontWeight(QFont::Bold);
+    textFormats["video"] = VideoFormat;
+
+    //image
+    QTextCharFormat ImageFormat;
+    ImageFormat.setBackground(QColor(194, 192, 65));
+    textFormats["image"] = ImageFormat;
+
+    //audio
+    QTextCharFormat AudioFormat;
+    AudioFormat.setBackground(QColor(149, 89, 170));
+    textFormats["audio"] = AudioFormat;
+
+    //GIF
+    QTextCharFormat GifFromat;
+    GifFromat.setBackground(QColor(107, 170, 89));
+    textFormats["gif"] = GifFromat;
+
+    //Effect
+    QTextCharFormat EffectFormat;
+    EffectFormat.setBackground(QColor(76, 80, 206));
+    EffectFormat.setFontUnderline(true);
+    textFormats["effect"] = EffectFormat;
+
+    //note
+    QTextCharFormat NoteFormat;
+    NoteFormat.setBackground(QColor(128, 128, 128));
+    NoteFormat.setFontItalic(true);
+    textFormats["note"] = NoteFormat;
 }
